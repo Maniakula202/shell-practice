47bd38d339552f744c5a1ef5dace93692b6b993e
Manikanta Akula <maniakula202@gmail.com>
2025-09-18 21:46:04 +0530

    added some changes

--- 02.second-file.sh
+++ 02.second-file.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-echo "Ramesh: hi bro how are you"
-echo "Manikanta: Hi bro.... I am doing good, what about you"
-echo "I am good mani, what were you doing"
-echo "Nothing ramesh bro"
+
+# I am creating variables for the names 
+person1=Ramesh
+person2=Mani
+
+echo "$person1: hi bro how are you"
+echo "$person2: Hi bro.... I am doing good, what about you"
+echo "$person1 I am good $person2, what were you doing"
+echo "Nothing $person1 bro"
