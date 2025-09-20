cd4d014a54e2a54361184a36db6c8c6e8961e1b9
Manikanta Akula <maniakula202@gmail.com>
2025-09-20 19:26:39 +0530

    variables practies

---
+++ variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+
+person1=$1
+person2=$2
+
+echo "$person1: hi bro how are you"
+echo "$person2: Hi bro.... I am doing good, what about you"
+echo "$person1 I am good $person2, what were you doing"
+echo "Nothing $person1 bro"
