abfacbc35569038b2cb3a3c3fff59dcfb1db23b0
Manikanta Akula <maniakula202@gmail.com>
2025-09-23 19:59:21 +0530

    Checking the loops

--- 18-loops.sh
+++ 18-loops.sh
@@ -8,4 +8,29 @@ N="\e[0m"
 
 if [ $USER_ID -ne 0 ]; then 
     echo "Error:: Please use the root privelege to install this files"
+    exit 1
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "Installing mysql ... $R FAILURE $N"
+else
+    echo "Installing mysql ... $R SUCCESS $N"
+fi
+
+dnf install nginx -y
+
+if [ $? -ne 0 ]; then
+    echo "Installing nginx ... $R FAILURE $N"
+else
+    echo "Installing nginx ... $R SUCCESS $N"
+fi
+
+dnf install python3 -y
+
+if [ $? -ne 0 ]; then
+    echo "Installing python3 ... $R FAILURE $N"
+else
+    echo "Installing python3 ... $R SUCCESS $N"
 fi
