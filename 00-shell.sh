d86b668d6d09190ad58e37f85cb42ea9a15b1553
Manikanta Akula <maniakula202@gmail.com>
2025-10-02 21:14:56 +0530

    ansible practice

--- 00-shell.sh
+++ 00-shell.sh
@@ -1,5 +1,26 @@
 #!/bin/bash 
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
-output=$(df -h | awk '{print $5}' | cut -d "%" -f1)
-echo "$output"
+inputfile=$1
+
+
+if [ ! -f $inputfile ]; then 
+    echo -e "$R Source directory $inputfile does not exits $N"
+else
+    echo -e "$G Source directory $inputfile Existed $N"
+
+if [ -r $inputfile ]; then
+  echo "Directory is readable "
+else
+  echo "Directory is not readable"
+fi
+
+if [ -w $inputfile ]; then
+  echo "File is writable"
+else
+  echo "File is not writable"
+fi
