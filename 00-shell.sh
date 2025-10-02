b1d9fa225462ad5c151d397827cae98dfd882932
Manikanta Akula <maniakula202@gmail.com>
2025-10-02 21:21:55 +0530

    ansible practice

--- 00-shell.sh
+++ 00-shell.sh
@@ -1,27 +1,14 @@
 #!/bin/bash 
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
 inputfile=$1
 
 
-if [ ! -e $inputfile ]; then 
-    echo -e "$R Source directory $inputfile does not exits $N"
+if [ -d $inputfile ]; then 
+    echo "The given input path is directory"
+    cd $inpufile
+    DIR_FILES=$(ls)
+    echo "$DIR_FILES"
 else
     echo -e "$G Source directory $inputfile Existed $N"
 fi 
 
-if [ -r $inputfile ]; then
-  echo "Directory is readable "
-else
-  echo "Directory is not readable"
-fi
-
-if [ -w $inputfile ]; then
-  echo "File is writable"
-else
-  echo "File is not writable"
-fi
